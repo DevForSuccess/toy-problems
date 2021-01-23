1b78ae0343b7c68201dd3acb61e26904f64c053c
VirtCoder <joekic@gmail.com>
2021-01-23 12:57:23 -0500

    fixed some variable referencing mistake, function also workss for array as input element

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -27,21 +27,28 @@ console.log('C: ', 'disregard duplicates value in the first input');
 console.log('E', 'make the method work for arrays that contain objects or arrays as elements');
 
 Array.prototype.isSubsetOf = function (arr) {
-  for (let i=0; i<this.length; i++) {
+  for (let i = 0; i < this.length; i++) {
     let arg = this[i];
-    if (arg.indexOf(arg) === -1) {
+    if (arr.indexOf(arg) === -1) {
       return false;
     }
+    // }
   }
+  let why = 'here';
   return true;
 };
 
-let a = ['commit','push'];
+let a = ['commit', 'push'];
 a.prototype = Array.prototype;
-let result = a.isSubsetOf(['commit','rebase','push','blame']); // true
-console.log(result);
+let result = a.isSubsetOf(['commit', 'rebase', 'push', 'blame']); // true
+console.log('result of a: ', result);
 
-let b = ['merge','reset','reset'];
+let b = ['merge', 'reset', 'reset'];
 b.prototype = Array.prototype;
-b.isSubsetOf(['reset','merge','add','commit']) // true
-console.log(result);
+result = b.isSubsetOf(['reset', 'merge', 'add', 'commit']) // true
+console.log('result of b: ', result);
+
+let c = ['car', 'house', ['coffee', 'hot chocolate']];
+c.prototype = Array.prototype;
+result = c.isSubsetOf(['reset', 'merge', 'add', 'commit', 'car', 'house', ['coffee', 'hot chocolate']]) // true
+console.log('result of c: ', result);
