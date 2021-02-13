8026372d9178dbe3dfa2193582ab01d605ef2033
Joe <joekic@gmail.com>
2021-02-13 12:26:22 -0500

    problem completed with extra credit

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,6 +8,21 @@
  */
 
 
-var largestProductOfThree = function(array) {
-  // TODO: everything
+var largestProductOfThree = function (array) {
+  let dup = array.slice().sort(function (a, b) {
+    return b - a;
+  });
+  let total = 1;
+  let ct = 0;
+  while (ct < 3 && dup[ct]) {
+    total *= dup[ct];
+    ct++;
+  }
+
+  return total;
 };
+let result = largestProductOfThree([2, 1, 3, 7]); //  === 42
+console.log(result);
+
+result = largestProductOfThree([2, -5, 3, -2]); //  === -12
+console.log(result);
