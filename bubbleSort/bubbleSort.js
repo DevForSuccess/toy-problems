8129ea2be020d1645b90b3426b1ef81c956fca93
Joe <joekic@hotmail.com>
2023-10-14 15:37:18 -0400

    practice bubble sort

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -14,14 +14,22 @@
  *
  * QUERY: What's the time complexity of your algorithm? If you don't already
  * know, try to intuit this without consulting the Googles.
+ * Answer: O(n^2) where n = number element - 1
  *
  * Extra credit: Optimization time! During any given pass, if no elements are
  * swapped we can assume the list is sorted and can exit the function early.
  * After this optimization, what is the time complexity of your algorithm?
+ * Answer: worst case (reverse order: O(n^2)
+ *         best case (array is already sorted): O(n-1)
  *
  * Moar credits: Do you need to consider every element every time you iterate
  * through the array? Make it happen, boss. Again: Has the time complexity of
  * your algorithm changed?
+ * Answer: no, at completion of each iteration, the higher number is already
+ *         at its rightmost correct position so we don't need to compare against
+ *         it anymore.
+ *         yes, the complexity becomes O(n/2(n+1)) where n = number element - 1
+ *
 */
 
 /*
@@ -35,21 +43,61 @@
 // for each element in the array.
 
 var bubbleSort = function (array) {
+  let n = 0;
+  let isSwap = false;
+  let swapCount = 0;
+
   for (let i = 0; i < array.length - 1; i++) {
-    let anySwap = false;
-    for (let j = 0; j < array.length - 1 - i; j++) {
+    isSwap = false;
+    for (let j = 0; j < array.length - 1; j++) {
+      n++;
       if (array[j] > array[j + 1]) {
+        isSwap = true;
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
-        anySwap = true;
       }
+      if (isSwap) swapCount++;
+      // console.log(`${n}: ${array}`);
     }
-    if (!anySwap) break;
+    if (!isSwap) break;
   }
+  console.log("number of iteration: ", n);
   return array;
 }
 
-var test = [1, 6, 34, 0, 9];
+var bubbleSortOptimized = function (array) {
+  let n = 0;
+  let isSwap = false;
+  let swapCount = 0;
+  let temp;
+  let arrLength = array.length - 1;
 
-console.log(bubbleSort(test));
+  do {
+    temp = null;
+    isSwap = false;
+    for (let j = 0; j < arrLength; j++) {
+      n++;
+      if (array[j] > array[j + 1]) {
+        isSwap = true;
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+      if (isSwap) swapCount++;
+      console.log(`${n}: ${array} => pointer: ${temp}`);
+    }
+    // decrease array length; last element is already the greatest
+    arrLength--;
+    console.log("array length: ", arrLength);
+    // if (!isSwap) break;
+  } while (arrLength > 0)
+  console.log("number of iteration: ", n);
+  return array;
+}
+
+// var test = [3, 2, 1, 0];
+var test = [7, 6, 5, 4, 3, 2, 1];
+// var test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(bubbleSortOptimized(test));
